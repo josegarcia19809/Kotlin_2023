8178c686cecf5a1c313ecee2529ad9ffe60b9e07
josegarcia19809 <josegarcia19809@gmail.com>
2024-06-01 19:34:27 -0600

    Uso de cadenas. Ejercicios

--- src/main/kotlin/k08_cadenas/StringFunctions.kt
+++ src/main/kotlin/k08_cadenas/StringFunctions.kt
@@ -30,4 +30,18 @@ fun main() {
     val diasTrabajados = 5
     val salarioPorDia = 250
     println("Tu salario es de $${salarioPorDia * diasTrabajados} pesos")
+
+    // Ejercicios:
+    // 1. Extraer la longitud de la frase: "Cuando pierdas, no pierdas la lección."
+    // 2. Extraer la palabra "pierdas" de la frase anterior
+    // 3. Cuando el nombre de un cliente en una variable, después imprime un mensaje personalizado
+    // para ese cliente.
+    // 4. Usa 2 variables, una para guardar la cantidad de manzanas y otra para la cantidad de peras
+    // después imprime el total de frutas que se tienen.
+    // 5. A client has purchased a number of products that have a certain price
+    // Print out the total amount that the client needs to pay
+    //
+    // val client = “Mary”
+    // val products = 3
+    // val price = 30
 }
