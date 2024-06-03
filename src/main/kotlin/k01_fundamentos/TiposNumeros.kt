22f845700fd29179d206cd990cea049446933384
josegarcia19809 <josegarcia19809@gmail.com>
2024-06-02 19:54:31 -0600

    Tipos de números

---
+++ src/main/kotlin/k01_fundamentos/TiposNumeros.kt
@@ -0,0 +1,15 @@
+package k01_fundamentos
+
+fun main() {
+    val numero1 = 45
+    println(numero1)
+    println(numero1::class.java)
+
+    val numero2 = 4599987777
+    println(numero2)
+    println(numero2::class.java)
+
+    val numero3 = 45.99
+    println(numero3)
+    println(numero3::class.java)
+}
