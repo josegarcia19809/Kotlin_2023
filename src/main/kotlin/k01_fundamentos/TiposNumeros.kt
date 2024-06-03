e4457186cd91d8c2ca42522ecb2d4c51c86de08d
josegarcia19809 <josegarcia19809@gmail.com>
2024-06-02 20:16:06 -0600

    Más tipos de números. Declaración explicita e implícita

--- src/main/kotlin/k01_fundamentos/TiposNumeros.kt
+++ src/main/kotlin/k01_fundamentos/TiposNumeros.kt
@@ -12,4 +12,16 @@ fun main() {
     val numero3 = 45.99
     println(numero3)
     println(numero3::class.java)
+
+    val numero4: Byte = 5
+    println(numero4)
+    println(numero4::class.java)
+
+    val numero5 = 5f
+    println(numero5)
+    println(numero5::class.java)
+
+    val numero6 = 6L
+    println(numero6)
+    println(numero6::class.java)
 }
