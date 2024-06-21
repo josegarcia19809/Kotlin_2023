d55c0539125f1dd2e7c132a13bdfa17d5503c60b
josegarcia19809 <josegarcia19809@gmail.com>
2024-06-20 21:33:23 -0600

    Menciona si un elemento existe o no en una colección

---
+++ src/main/kotlin/k07_mas_colecciones/In_Keyword.kt
@@ -0,0 +1,29 @@
+package k07_mas_colecciones
+
+fun main() {
+    val favouritePet = "dog"
+    val availablePets = listOf("dog", "cat", "horse")
+
+    if (favouritePet in availablePets) { // si está el elemento en la lista
+        println("We have your next best friend")
+    } else {
+        println("Sorry, $favouritePet is not available")
+    }
+
+
+    println("Enter your preferred coffe: ")
+    val coffe = readln()
+    val availableCoffees = listOf("capuccino", "mocha")
+    if (coffe !in availableCoffees) { // Si no está el elemento en la lista
+        println("Sorry, $coffe is not available")
+    } else {
+        println("$coffe is available")
+    }
+
+    println("Please, enter a number: ")
+    val num = readln().toInt()
+    if (num !in 1..9){
+        println("Your number is not single digit")
+    }
+
+}
