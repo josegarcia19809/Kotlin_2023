fc84f7ca8067ccd81ee172edaf414bf97950a8d9
josegarcia19809 <josegarcia19809@gmail.com>
2024-06-26 21:53:50 -0600

    Al añadir un elemento a una lista devuelve un valor booleano

--- src/main/kotlin/k07_mas_colecciones/Boolean_Functions.kt
+++ src/main/kotlin/k07_mas_colecciones/Boolean_Functions.kt
@@ -1,9 +1,14 @@
 package k07_mas_colecciones
 
-fun main(){
+fun main() {
     val animals = arrayListOf<String>()
-    if (animals.isEmpty()){
+    if (animals.isEmpty()) {
         animals.add("dog")
     }
     println(animals)
+
+    val otherAnimals = arrayListOf<String>()
+    if (otherAnimals.add("dog")) {
+        println("Item was successfully added")
+    }
 }
