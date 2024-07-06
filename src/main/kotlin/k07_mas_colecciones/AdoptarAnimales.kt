d2c3567ea683b6d240da0837338b176788df644c
josegarcia19809 <josegarcia19809@gmail.com>
2024-07-05 21:31:31 -0600

    Adoptar a un animal: condición y arrayListOf

---
+++ src/main/kotlin/k07_mas_colecciones/AdoptarAnimales.kt
@@ -0,0 +1,19 @@
+package k07_mas_colecciones
+
+/*
+* An animal shelter has a list of animals that it tries to get adopted.
+* They are trying to print a message, to either advertise available animals
+* if there are any animals in the shelter, or thank the community if all
+* animals have been adopted.
+*
+* Design a program to print out that message
+* */
+
+fun main() {
+    val animals = arrayListOf("cat", "dog")
+    if (animals.isEmpty()) {
+        println("Thank you for giving homes to all our pets")
+    } else {
+        println("Please come in and adopt a pet. We have $animals")
+    }
+}
