7d1e1e36a1f372568406756ae0fff9abac6d9d8c
josegarcia19809 <josegarcia19809@gmail.com>
2024-07-06 20:18:51 -0600

    Mensaje según edad: rangos

---
+++ src/main/kotlin/k07_mas_colecciones/MensajeSegunEdad.kt
@@ -0,0 +1,31 @@
+package k07_mas_colecciones
+
+/*
+* Ask a user to input their age.
+
+If they are under 13, they are a child
+If they are under 18, they are a teen
+If they are older, they are an adult.
+
+Use ranges to print out the correct message.
+
+If the age is 0, convert it to 1.
+* */
+
+fun main() {
+    print("Please enter your age: ")
+    var age = readLine()!!.toInt()
+
+    if (age == 0) age = 1
+
+    val result = if (age in 1..12) {
+        "a child"
+    } else if (age in 13..17) {
+        "a teen"
+    } else {
+        "an adult"
+    }
+
+    println("User is $result")
+
+}
