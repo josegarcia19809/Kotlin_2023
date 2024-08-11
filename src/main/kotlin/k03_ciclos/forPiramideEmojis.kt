071f6f44c955ba3d191abf1a0acb425bf840c711
josegarcia19809 <josegarcia19809@gmail.com>
2024-08-11 13:28:56 -0600

    Ciclos for: Pirámide de caritas

---
+++ src/main/kotlin/k03_ciclos/forPiramideEmojis.kt
@@ -0,0 +1,13 @@
+package k03_ciclos
+
+/*
+* Piramide de emojis
+* */
+fun main() {
+    for (i in 1..10) {
+        for (j in 1..i) {
+            print("😬")
+        }
+        println()
+    }
+}
