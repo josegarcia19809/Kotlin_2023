dc6b20d027d5c4e5b849b192c4d9949949fa4921
josegarcia19809 <josegarcia19809@gmail.com>
2024-08-17 22:02:48 -0600

    ciclos while: Piezas colocadas

---
+++ src/main/kotlin/k10_clases/while_piezas_colocadas.kt
@@ -0,0 +1,11 @@
+package k10_clases
+
+fun main() {
+    var puzzlePieces = 20
+    var piecesPlaced = 0
+
+    while (piecesPlaced < puzzlePieces) {
+        piecesPlaced++
+        println("Placed piece #$piecesPlaced")
+    }
+}
