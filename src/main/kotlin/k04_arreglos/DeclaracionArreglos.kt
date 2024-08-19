98d4a5b6cbd187c13f9b0b3dd15ccc5e99179438
josegarcia19809 <josegarcia19809@gmail.com>
2024-08-19 15:57:42 -0600

    Arreglos: Declaración

---
+++ src/main/kotlin/k04_arreglos/DeclaracionArreglos.kt
@@ -0,0 +1,10 @@
+package k04_arreglos
+
+fun main() {
+    val numbers: Array<Int> = Array(6) { 0 }
+    numbers[0] = 20
+    numbers[3] = 30
+    for (i in 0..5) {
+        print(" ${numbers[i]}")
+    }
+}
