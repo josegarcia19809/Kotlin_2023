f01159edae0a89b7f312a4f89da09231ba0044b0
josegarcia19809 <josegarcia19809@gmail.com>
2024-08-24 15:56:58 -0600

    ciclos while: suma de números usando un valor centinela

---
+++ src/main/kotlin/k03_ciclos/while_suma_numeros.kt
@@ -0,0 +1,14 @@
+package k03_ciclos
+
+fun main(args: Array<String>) {
+    //suma de números usando valor centinela
+    var suma = 0;
+    print("Introduce un número (0 para terminar): ")
+    var numero = readln().toInt()
+    while (numero != 0) {
+        suma += numero
+        print("Introduce un número (0 para terminar): ")
+        numero = readln().toInt()
+    }
+    println("La suma es $suma")
+}
