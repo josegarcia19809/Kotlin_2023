089d6cd93a9bcf93e1283ea587779966e9ca3622
josegarcia19809 <josegarcia19809@gmail.com>
2024-10-23 21:10:19 -0600

    POO: Actualización

--- src/main/kotlin/k10_poo_clases/AnimalPractice.kt
+++ src/main/kotlin/k10_poo_clases/AnimalPractice.kt
@@ -1,5 +1,10 @@
 package k10_poo_clases
-
+/*
+* Crea una clase llamada Animal. Tiene una variable topSpeed y una función run() que
+* imprime un mensaje indicando la velocidad máxima del animal.
+Crea un objeto de esa clase y ejecuta la función.
+Actualiza la variable y ejecuta la función nuevamente.
+* */
 class Animal {
     var name = ""
     var topSpeed = 0
