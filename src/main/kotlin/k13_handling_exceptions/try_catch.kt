5d5df6d80ccfdf691d44c938f2ef91abeb33e2d4
josegarcia19809 <josegarcia19809@gmail.com>
2024-11-25 07:18:28 -0600

    Handling exceptions: Try catch, ex.localizedMessage,  ex.printStackTrace()

--- src/main/kotlin/k13_handling_exceptions/try_catch.kt
+++ src/main/kotlin/k13_handling_exceptions/try_catch.kt
@@ -1,10 +1,17 @@
 package k13_handling_exceptions
 
+import k05_funciones.imprimirLinea
+
 fun main() {
+    println("Ingresa un número: ")
     val test = readLine()
     try {
         println(test?.toInt())
     } catch (ex: Exception) {
         println("A exception occurred")
+        println(ex.localizedMessage)
+        imprimirLinea()
+        ex.printStackTrace()
+        imprimirLinea()
     }
 }
