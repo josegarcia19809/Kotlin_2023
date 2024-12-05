6412f1a82fb339aa732b3fb4e5b710f7b8514047
josegarcia19809 <josegarcia19809@gmail.com>
2024-12-05 11:46:17 -0600

    POO: init - WebBrowser

---
+++ src/main/kotlin/k10_poo_clases/WebBrowser_init.kt
@@ -0,0 +1,12 @@
+package k10_poo_clases
+
+class WebBrowser {
+    init {
+        println("Web browser is connecting...")
+        println("https://www.google.com")
+    }
+}
+
+fun main() {
+    val webBrowser = WebBrowser()
+}
